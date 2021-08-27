4deb38a7c3b47206550cc7d2f931cfaebf277179
fv00 <sfrancov@unal.edu.co>
2021-08-26 23:03:32 -0500

    Create carrosAntioquia.py

---
+++ carrosAntioquia.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Setting soup
+url = "https://carros.mercadolibre.com.co/renault/antioquia/renault-logan_DisplayType_LF"
+req = requests.get(url)
+soup = BeautifulSoup(req.content, features="html.parser")
+
+# Setting desired classes
+first_class = ["ui-search-item__title"]
+second_class = ["price-tag-fraction"]
+third_class = ["ui-search-card-attributes__attribute"]
+
+# Getting names
+names = soup.find_all(class_=first_class)
+names = [x.contents[0] for x in names]
+
+# Getting prices
+prices = soup.find_all(class_=second_class)
+prices = [x.contents[0] for x in prices]
+
+years = soup.find_all(class_=third_class)
+years =  [x.contents[0] for x in years if x.contents[0].isdigit()]
+
+# Dropping slider prices
+prices.pop(0)
+prices.pop(0)
+
+# Saving results on a csv
+to_save = pd.DataFrame({
+    "Nombre": names,
+    "Modelo": years,
+    "Precio": prices
+})
+
+to_save.to_csv("carrosAntioquia.csv", index = False)
